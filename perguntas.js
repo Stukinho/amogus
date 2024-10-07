e088a0548fc172fab6c93c8fdc5072ef625b0e5d
Stukinho <142433899+Stukinho@users.noreply.github.com>
2024-10-07 12:03:10 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,84 @@
+criaCartao(
+'Matematica',
+'Qual é a raiz quadrada de 144?',
+'12')
+
+criaCartao(
+'Quimica',
+'Quantos carbonos tem em uma cadeia de Cafeina?',
+'8')
+
+criaCartao(
+'Geografia',
+'Em qual continente fica Madagascar?',
+'Africa')
+
+criaCartao(
+'Historia',
+'Em que ano Adolf Hitler morreu?',
+'1945')
+
+criaCartao(
+'Biologia',
+'O que faz uma organela?',
+'faz a digestão intracelular')
+
+criaCartao(
+'Português',
+'Quais são as características do realismo?',
+'Valorização da objetividade e dos fatos. Impessoalidade, apagamento das ideias do autor.')
+
+criaCartao(
+'Biologia',
+'',
+'')
+
+criaCartao(
+'Matematica',
+'Qual é o nome da função de primeiro grau?',
+'Função Afim')
+    
+criaCartao(
+'Quimica',
+'Porque ocorre uma ligação covalente?',
+'ocorre para que cada átomo atinja sua estabilidade, com presença de oito elétrons em sua camada de valência.')
+    
+criaCartao(
+'Geografia',
+'Qual é a primeira capital do Brasil?',
+'Salvador')
+    
+criaCartao(
+'Historia',
+'Em que ano ocorreu a primeira revolução industrial?',
+'1760')
+    
+criaCartao(
+'Biologia',
+'quanto tempo em media uma mosca vive?',
+'28 dias')
+    
+criaCartao(
+'Português',
+'Quais são as características do realismo?',
+'')
+
+criaCartao(
+'Matematica',
+'Qual é a raiz quadrada de 144?',
+'12')
+        
+criaCartao(
+'Quimica',
+'Quantos carbonos tem em uma cadeia de Cafeina?',
+'8')
+        
+criaCartao(
+'Geografia',
+'Em qual continente fica Madagascar?',
+'Africa')
+        
+criaCartao(
+'Historia',
+'Em que ano Adolf Hitler morreu?',
+'1945')
