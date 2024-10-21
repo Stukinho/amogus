0af03a2724d31967c2b2da019aa7cbff4042db3e
Stukinho <142433899+Stukinho@users.noreply.github.com>
2024-10-21 09:08:19 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,84 +1,99 @@
 criaCartao(
+'Geografia',
+'Questão 13: Qual é a primeira capital do Brasil?',
+'Salvador')
+        
+criaCartao(
+'Historia',
+'Questão 14: Em que ano ocorreu a primeira revolução industrial?',
+'1760')
+        
+criaCartao(
+'Matemática',
+'Questão 15: Como é representado uma incógnita na matemática?',
+'É representado com "x"')
+
+criaCartao(
 'Matematica',
-'Qual é a raiz quadrada de 144?',
+'Questão 4: Qual é a raiz quadrada de 144?',
 '12')
 
 criaCartao(
 'Quimica',
-'Quantos carbonos tem em uma cadeia de Cafeina?',
+'Questão 5: Quantos carbonos tem em uma cadeia de Cafeina?',
 '8')
 
 criaCartao(
 'Geografia',
-'Em qual continente fica Madagascar?',
+'Questão 6: Em qual continente fica Madagascar?',
 'Africa')
 
 criaCartao(
 'Historia',
-'Em que ano Adolf Hitler morreu?',
+'Questão 7: Em que ano Adolf Hitler morreu?',
 '1945')
 
 criaCartao(
 'Biologia',
-'O que faz uma organela?',
+'Questão 8: O que faz uma organela?',
 'Faz a digestão intracelular')
 
 criaCartao(
 'Português',
-'Quais são as características do realismo?',
+'Questão 9: Quais são as características do realismo?',
 'Valorização da objetividade e dos fatos. Impessoalidade, apagamento das ideias do autor.')
 
 criaCartao(
 'Biologia',
-'Qual o nome do orgão que sua principal tarefa é filtrar o sangue?',
+'Questão 10: Qual o nome do orgão que sua principal tarefa é filtrar o sangue?',
 'Rim')
 
 criaCartao(
 'Matematica',
-'Qual é o nome da função de primeiro grau?',
+'Questão 11: Qual é o nome da função de primeiro grau?',
 'Função Afim')
     
 criaCartao(
 'Quimica',
-'Porque ocorre uma ligação covalente?',
+'Questão 12: Porque ocorre uma ligação covalente?',
 'ocorre para que cada átomo atinja sua estabilidade, com presença de oito elétrons em sua camada de valência.')
     
 criaCartao(
 'Geografia',
-'Qual é a primeira capital do Brasil?',
+'Questão 13: Qual é a primeira capital do Brasil?',
 'Salvador')
     
 criaCartao(
 'Historia',
-'Em que ano ocorreu a primeira revolução industrial?',
+'Questão 14: Em que ano ocorreu a primeira revolução industrial?',
 '1760')
     
 criaCartao(
 'Matemática',
-'Como é representado uma incógnita na matemática?',
+'Questão 15: Como é representado uma incógnita na matemática?',
 'É representado com "x"')
     
 criaCartao(
 'Português',
-'O que é a metáfora?',
+'Questão 16: O que é a metáfora?',
 'é uma figura de linguagem; mais particularmente, uma figura de palavra, pois apresenta uma palavra (ou expressão) com sentido figurado.')
 
 criaCartao(
 'Matematica',
-'Qual o valor de PI?',
+'Questão 17: Qual o valor de PI?',
 '3,14')
         
 criaCartao(
 'Quimica',
-'Qual cientista propôs o primeiro modelo atômico moderno que ficou conhecido como “bola de bilhar”?',
+'Questão 18: Qual cientista propôs o primeiro modelo atômico moderno que ficou conhecido como “bola de bilhar”?',
 'John Dalton')
         
 criaCartao(
 'Geografia',
-'Como é causado um terremoto?',
+'Questão 19: Como é causado um terremoto?',
 'São provocados por movimentos na crosta terrestre, composta por enormes placas de rocha, chamadas de placas tectônicas.')
         
 criaCartao(
 'Historia',
-'Quanto tempo durou a Era Vargas?',
+'Questão 20: Quanto tempo durou a Era Vargas?',
 '15 Anos')
