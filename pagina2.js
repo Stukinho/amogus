e6482ec4481ed9f11ea49abf705e60dae308ad39
Stukinho <142433899+Stukinho@users.noreply.github.com>
2024-11-04 10:24:25 -0300

    .

--- pagina2.js
+++ pagina2.js
@@ -1,8 +1,8 @@
-function criaCartao (categoria, pergunta, resposta){
-   let container = document.getElementById('container');
-   let cartao = document.createElement('article');
-   cartao.className = 'cartao';
-   cartao.innerHTML = `
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
    <div class="conteudo-cartao" onclick = "viraCartao(this)">
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
@@ -13,14 +13,14 @@ function criaCartao (categoria, pergunta, resposta){
     </div>
 </div>
    `
-   container.appendChild(cartao);
+    container.appendChild(cartao);
 }
-function viraCartao(card){
+function viraCartao(card) {
     const resposta = card.querySelector('.resposta-cartao');
     if (resposta.style.display === 'none') {
         resposta.style.display = 'block';
     }
     else {
-resposta.style.display = 'none';
+        resposta.style.display = 'none';
     }
 }
