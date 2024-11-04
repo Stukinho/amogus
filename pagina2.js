8b0de563d95a47e2421d31b929e77f1f207f5bf6
Stukinho <142433899+Stukinho@users.noreply.github.com>
2024-11-04 09:32:07 -0300

    .

--- pagina2.js
+++ pagina2.js
@@ -4,6 +4,7 @@ function criaCartao (categoria, pergunta, resposta){
    cartao.className = 'cartao';
    cartao.innerHTML = `
    <div class="conteudo-cartao">
+   onclick = "viraCartao(this)"
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
         <p>${pergunta}</p>
